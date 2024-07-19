79eedeefb89aac91e4c7dd1f4abcca08cac47a01
Andrei Andreev <razenpok@outlook.com>
2024-07-20 00:45:55 +0200

    Rewrite header-calculator-button

--- browser-extension/src/refined-prun.ts
+++ browser-extension/src/refined-prun.ts
@@ -1,6 +1,7 @@
 import './features/xit-commands';
 import './features/flt-arrival-eta';
 import './features/sfc-flight-eta';
+import './features/header-calculator-button';
 import './features/correct-planet-command';
 import './features/evaluate-input-formula';
 import './features/reload-page-on-websocket-disconnect';
