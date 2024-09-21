9fc5ff2f5cd1147cb440ec24131656e45d3bdade
Andrei Andreev <razenpok@outlook.com>
2024-09-21 17:25:08 +0200

    Add to/from to tile state in XIT HQUC

--- browser-extension/src/features/XIT/HQUC.vue
+++ browser-extension/src/features/XIT/HQUC.vue
@@ -12,13 +12,20 @@ export default {};
 </script>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { calculateHQUpgradeMaterials, maxHQLevel } from '@src/core/hq';
 import { companyStore } from '@src/infrastructure/prun-api/data/company';
 import MaterialPurchaseTable from '@src/components/MaterialPurchaseTable.vue';
+import { useTileState } from '@src/store/user-data-tiles';
+import PrunButton from '@src/components/PrunButton.vue';
 
-const from = ref(companyStore.value?.headquarters.level ?? 1);
-const to = ref(from.value + 1);
+const from = useTileState('from', companyStore.value?.headquarters.level ?? 1);
+const to = useTileState('to', from.value + 1);
+
+function reset() {
+  from.value = companyStore.value?.headquarters.level ?? 1;
+  to.value = from.value + 1;
+}
 
 const materials = computed(() => calculateHQUpgradeMaterials(from.value, to.value));
 </script>
@@ -34,6 +41,7 @@ const materials = computed(() => calculateHQUpgradeMaterials(from.value, to.valu
       <select v-model="to">
         <option v-for="i in maxHQLevel" :key="i">{{ i }}</option>
       </select>
+      <PrunButton primary @click="reset">RESET</PrunButton>
     </div>
     <MaterialPurchaseTable :materials="materials" />
   </div>
