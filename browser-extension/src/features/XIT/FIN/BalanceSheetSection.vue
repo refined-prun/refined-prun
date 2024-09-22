cd9783f3892206495b6d2a06d2473e27750959eb
Andrei Andreev <razenpok@outlook.com>
2024-09-22 10:09:50 +0200

    Fix XIT FINBS failing for missing data points

--- browser-extension/src/features/XIT/FIN/BalanceSheetSection.vue
+++ browser-extension/src/features/XIT/FIN/BalanceSheetSection.vue
@@ -24,12 +24,16 @@ const props = defineProps({
   },
 });
 
+function calculate(
+  sheet: PartialBalanceSheet | undefined,
+  selector: (x: PartialBalanceSheet) => number | undefined,
+) {
+  return sheet !== undefined ? selector(sheet) : undefined;
+}
+
 function calculateChange(selector: (x: PartialBalanceSheet) => number | undefined) {
-  if (!props.last || !props.previous) {
-    return undefined;
-  }
-  const fromLast = selector(props.last);
-  const fromPrevious = selector(props.previous);
+  const fromLast = calculate(props.last, selector);
+  const fromPrevious = calculate(props.previous, selector);
   if (fromLast === undefined || fromPrevious === undefined || fromPrevious === 0) {
     return undefined;
   }
@@ -59,16 +63,16 @@ const totalClass = computed(() => {
     </tr>
     <tr v-for="row in section.rows" :key="row[0]">
       <td>{{ row[0] }}</td>
-      <td>{{ formatAmount(row[1](current)) }}</td>
-      <td>{{ formatAmount(row[1](last)) }}</td>
-      <td>{{ formatAmount(row[1](previous)) }}</td>
+      <td>{{ formatAmount(calculate(current, row[1])) }}</td>
+      <td>{{ formatAmount(calculate(last, row[1])) }}</td>
+      <td>{{ formatAmount(calculate(previous, row[1])) }}</td>
       <td>{{ formatChange(calculateChange(row[1])) }}</td>
     </tr>
     <tr :class="PrunCss.IncomeStatementPanel.totals">
       <td :class="PrunCss.IncomeStatementPanel.number">Total</td>
-      <td>{{ formatAmount(section.total(current)) }}</td>
-      <td>{{ formatAmount(section.total(last)) }}</td>
-      <td>{{ formatAmount(section.total(previous)) }}</td>
+      <td>{{ formatAmount(calculate(current, section.total)) }}</td>
+      <td>{{ formatAmount(calculate(last, section.total)) }}</td>
+      <td>{{ formatAmount(calculate(previous, section.total)) }}</td>
       <td :class="totalClass">{{ formatChange(calculateChange(section.total)) }}</td>
     </tr>
   </tbody>
