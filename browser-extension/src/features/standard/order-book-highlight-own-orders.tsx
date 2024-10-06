293bcd43b33e85498b5840ad52cb78906f7d9a75
Andrei Andreev <razenpok@outlook.com>
2024-10-07 00:16:33 +0200

    Fix order-book-highlight-own-orders not highlighting orders on subsequent opens

--- browser-extension/src/features/standard/order-book-highlight-own-orders.tsx
+++ browser-extension/src/features/standard/order-book-highlight-own-orders.tsx
@@ -5,7 +5,7 @@ import { companyStore } from '@src/infrastructure/prun-api/data/company';
 
 function onTileReady(tile: PrunTile) {
   const rows = tile.anchor.getElementsByTagName('tr');
-  const observer = new MutationObserver(() => {
+  const processRows = () => {
     for (const row of Array.from(rows)) {
       if (row.firstChild?.textContent === companyStore.value?.name) {
         row.classList.add(classes.highlight);
@@ -13,7 +13,9 @@ function onTileReady(tile: PrunTile) {
         row.classList.remove(classes.highlight);
       }
     }
-  });
+  };
+  processRows();
+  const observer = new MutationObserver(processRows);
   observer.observe(tile.anchor, { childList: true, subtree: true, characterData: true });
 }
 
