1a9919bbbaf5701ccb3981eb2d2c219f0d8afe6d
Andrei Andreev <razenpok@outlook.com>
2024-10-31 10:02:19 +0100

    Add SITE_PLATFORM messages

--- browser-extension/src/infrastructure/prun-api/data/sites.ts
+++ browser-extension/src/infrastructure/prun-api/data/sites.ts
@@ -17,6 +17,42 @@ messages({
   SITE_SITE(data: PrunApi.Site) {
     store.setOne(data);
   },
+  SITE_PLATFORM_BUILT(data: PrunApi.Platform) {
+    const entities = state.entities.value;
+    const site = entities?.[data.siteId];
+    if (entities === undefined || site === undefined) {
+      return;
+    }
+
+    entities[data.siteId] = {
+      ...site,
+      platforms: [...site.platforms, data],
+    };
+  },
+  SITE_PLATFORM_UPDATED(data: PrunApi.Platform) {
+    const entities = state.entities.value;
+    const site = entities?.[data.siteId];
+    if (entities === undefined || site === undefined) {
+      return;
+    }
+
+    entities[data.siteId] = {
+      ...site,
+      platforms: site.platforms.map(x => (x.id === data.id ? data : x)),
+    };
+  },
+  SITE_PLATFORM_REMOVED(data: { siteId: string; platformId: string }) {
+    const entities = state.entities.value;
+    const site = entities?.[data.siteId];
+    if (entities === undefined || site === undefined) {
+      return;
+    }
+
+    entities[data.siteId] = {
+      ...site,
+      platforms: site.platforms.filter(x => x.id !== data.platformId),
+    };
+  },
 });
 
 const getByShortId = createMapGetter(state.all, x => x.siteId.substring(0, 8));
