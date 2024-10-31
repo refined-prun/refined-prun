99bb4542987eebed38c57f83f1b3caa02c09d50f
Andrei Andreev <razenpok@outlook.com>
2024-10-31 15:25:49 +0100

    Hide zero columns in finla-exchange-deposits

--- browser-extension/src/features/standard/finla-exchange-deposits.tsx
+++ browser-extension/src/features/standard/finla-exchange-deposits.tsx
@@ -1,8 +1,9 @@
 import classes from './finla-exchange-deposits.module.css';
+import css from '@src/utils/css-utils.module.css';
 import tiles from '@src/infrastructure/prun-ui/tiles';
 import features from '@src/feature-registry';
 import { refTextContent } from '@src/utils/reactive-dom';
-import { computed } from 'vue';
+import { computed, Ref } from 'vue';
 import { fixed0 } from '@src/utils/format';
 import PrunCss from '@src/infrastructure/prun-ui/prun-css';
 import { applyCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
@@ -21,9 +22,11 @@ function onTableHeadReady(thead: HTMLTableSectionElement) {
     const cx = row.appendChild(document.createElement('th'));
     cx.textContent = 'CX Deposits';
     cx.classList.add(PrunCss.LiquidAssetsPanel.number);
+    hideCellIfZeroTotal(cx, currentAssets.cxDepositsTotal);
     const fx = row.appendChild(document.createElement('th'));
     fx.textContent = 'FX Deposits';
     fx.classList.add(PrunCss.LiquidAssetsPanel.number);
+    hideCellIfZeroTotal(fx, currentAssets.fxDepositsTotal);
   });
 }
 
@@ -40,19 +43,30 @@ function onTableBodyReady(tbody: HTMLTableSectionElement) {
       currency.value ? currentAssets.cxDeposits.value?.get(currency.value) ?? 0 : 0,
     );
     watchEffectWhileNodeAlive(row, () => (cx.textContent = fixed0(cxDeposits.value)));
+    hideCellIfZeroTotal(cx, currentAssets.cxDepositsTotal);
     const fx = row.appendChild(document.createElement('td'));
     fx.classList.add(PrunCss.LiquidAssetsPanel.number);
     const fxDeposits = computed(() =>
       currency.value ? currentAssets.fxDeposits.value?.get(currency.value) ?? 0 : 0,
     );
     watchEffectWhileNodeAlive(row, () => (fx.textContent = fixed0(fxDeposits.value)));
+    hideCellIfZeroTotal(fx, currentAssets.fxDepositsTotal);
+  });
+}
+
+function hideCellIfZeroTotal(cell: HTMLTableCellElement, total: Ref<number | undefined>) {
+  watchEffectWhileNodeAlive(cell.parentElement!, () => {
+    if ((total.value ?? 0) === 0) {
+      cell.classList.add(css.hidden);
+    } else {
+      cell.classList.remove(css.hidden);
+    }
   });
 }
 
 export function init() {
-  for (let i = 2; i < 5; i++) {
-    applyCssRule(`.${PrunCss.LiquidAssetsPanel.row} td:nth-child(${i})`, classes.row);
-  }
+  applyCssRule(`.${PrunCss.LiquidAssetsPanel.row} td:first-child`, classes.firstColumn);
+  applyCssRule(`.${PrunCss.LiquidAssetsPanel.row} td:not(:first-child)`, classes.otherColumns);
   tiles.observe('FINLA', onTileReady);
 }
 
