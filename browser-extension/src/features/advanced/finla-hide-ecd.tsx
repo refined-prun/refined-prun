d4e34e26eb58d62e1b302f4267e892f186b587ce
Andrei Andreev <razenpok@outlook.com>
2024-11-05 00:43:39 +0100

    Fix finla-hide-ecd hiding non-ECD rows

--- browser-extension/src/features/advanced/finla-hide-ecd.tsx
+++ browser-extension/src/features/advanced/finla-hide-ecd.tsx
@@ -1,10 +1,22 @@
-import css from '@src/utils/css-utils.module.css';
 import PrunCss from '@src/infrastructure/prun-ui/prun-css';
 import features from '@src/feature-registry';
-import { applyScopedCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
+import { subscribe } from '@src/utils/subscribe-async-generator';
+import { $$ } from '@src/utils/select-dom';
+import tiles from '@src/infrastructure/prun-ui/tiles';
+import { refTextContent } from '@src/utils/reactive-dom';
+import { watchEffectWhileNodeAlive } from '@src/utils/watch-effect-while-node-alive';
+
+function onTileReady(tile: PrunTile) {
+  subscribe($$(tile.anchor, PrunCss.LiquidAssetsPanel.row), row => {
+    const currency = refTextContent(row.children[0]);
+    watchEffectWhileNodeAlive(row, () => {
+      row.style.display = currency.value === 'ECD' ? 'none' : '';
+    });
+  });
+}
 
 export function init() {
-  applyScopedCssRule('FINLA', `.${PrunCss.LiquidAssetsPanel.row}:nth-child(4)`, css.hidden);
+  tiles.observe('FINLA', onTileReady);
 }
 
 void features.add({
