2b514018906586f9d4a440dbd3d095df22aac0d6
Andrei Andreev <razenpok@outlook.com>
2024-11-09 01:30:58 +0100

    Fix alternating row colors in FINLA when ECD is hidden

--- browser-extension/src/features/advanced/finla-hide-ecd.tsx
+++ browser-extension/src/features/advanced/finla-hide-ecd.tsx
@@ -1,16 +1,34 @@
 import PrunCss from '@src/infrastructure/prun-ui/prun-css';
 import features from '@src/feature-registry';
 import { subscribe } from '@src/utils/subscribe-async-generator';
-import { $$ } from '@src/utils/select-dom';
+import { $$, _$$ } from '@src/utils/select-dom';
 import tiles from '@src/infrastructure/prun-ui/tiles';
 import { refTextContent } from '@src/utils/reactive-dom';
 import { watchEffectWhileNodeAlive } from '@src/utils/watch-effect-while-node-alive';
+import { observeDescendantListChanged } from '@src/utils/mutation-observer';
 
 function onTileReady(tile: PrunTile) {
   subscribe($$(tile.anchor, PrunCss.LiquidAssetsPanel.row), row => {
     const currency = refTextContent(row.children[0]);
     watchEffectWhileNodeAlive(row, () => {
       row.style.display = currency.value === 'ECD' ? 'none' : '';
+    });
+  });
+  // Move the ECD column to the end to avoid breaking
+  // the alternating table row colors.
+  subscribe($$(tile.anchor, 'tbody'), tbody => {
+    observeDescendantListChanged(tbody, () => {
+      const rows = _$$(tbody, 'tr');
+      for (const row of rows) {
+        const currency = row.children[0].textContent;
+        if (currency !== 'ECD') {
+          continue;
+        }
+        if (row !== tbody.lastChild) {
+          tbody.appendChild(row);
+        }
+        return;
+      }
     });
   });
 }
