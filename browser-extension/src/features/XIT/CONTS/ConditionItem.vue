80e139f6d6f40006b6abc0705ce1e28a65555639
Andrei Andreev <razenpok@outlook.com>
2024-11-10 00:47:31 +0100

    Replace Unicode characters with Font Awesome in ConditionItem

--- browser-extension/src/features/XIT/CONTS/ConditionItem.vue
+++ browser-extension/src/features/XIT/CONTS/ConditionItem.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import fa from '@src/utils/font-awesome.module.css';
 import { computed, PropType } from 'vue';
 import { friendlyConditionText, isConditionFulfilled } from '@src/features/XIT/CONTS/utils';
 import PrunCss from '@src/infrastructure/prun-ui/prun-css';
@@ -15,11 +16,14 @@ const iconClass = computed(() => ({
   [PrunCss.ColoredValue.positive]: isFulfilled.value,
   [PrunCss.ColoredValue.negative]: !isFulfilled.value,
 }));
-const icon = computed(() => (isFulfilled.value ? '✓' : '✗'));
+const icon = computed(() => (isFulfilled.value ? '\uf00c' : '\uf00d'));
 </script>
 
 <template>
   <div>
-    <span :class="iconClass">{{ icon }}&nbsp;{{ friendlyConditionText(condition.type) }}</span>
+    <span :class="iconClass">
+      <span :class="[fa.solid]">{{ icon }}</span
+      >&nbsp;{{ friendlyConditionText(condition.type) }}
+    </span>
   </div>
 </template>
