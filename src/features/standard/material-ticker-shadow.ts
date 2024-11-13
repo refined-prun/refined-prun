2f4748650979585196c0e8961567351772ec5aec
Andrei Andreev <razenpok@outlook.com>
2024-11-13 18:04:53 +0100

    Add ticker shadow to building tickers

--- src/features/standard/material-ticker-shadow.ts
+++ src/features/standard/material-ticker-shadow.ts
@@ -3,6 +3,7 @@ import { applyClassCssRule } from '@src/infrastructure/prun-ui/refined-prun-css'
 
 function init() {
   applyClassCssRule(C.ColoredIcon.label, classes.shadow);
+  applyClassCssRule(C.BuildingIcon.ticker, classes.shadow);
 }
 
 features.add(import.meta.url, init, 'Adds a shadow to material tickers.');
