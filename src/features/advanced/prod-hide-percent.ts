06d4814b499d25dd3b426b1d86e6d07429bb7d28
CoroNaut <JavaSane2121@yahoo.com>
2025-02-15 07:09:47 -0500

    fixed imports and tested changes

--- src/features/advanced/prod-hide-percent.ts
+++ src/features/advanced/prod-hide-percent.ts
@@ -1,5 +1,5 @@
 import css from '@src/utils/css-utils.module.css';
-import { applyClassCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
+import { applyScopedClassCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
 
 function init() {
   applyScopedClassCssRule('PROD', C.OrderStatus.inProgress, css.hidden);
