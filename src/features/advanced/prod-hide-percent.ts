5d81792d64ec96ad4052bfdf57a01259164260eb
CoroNaut <JavaSane2121@yahoo.com>
2025-02-15 07:05:02 -0500

    Apply suggestions from code review
    Co-authored-by: Andrei Andreev <Razenpok@outlook.com>

--- src/features/advanced/prod-hide-percent.ts
+++ src/features/advanced/prod-hide-percent.ts
@@ -2,11 +2,11 @@ import css from '@src/utils/css-utils.module.css';
 import { applyClassCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
 
 function init() {
-  applyClassCssRule(C.OrderStatus.inProgress, css.hidden);
+  applyScopedClassCssRule('PROD', C.OrderStatus.inProgress, css.hidden);
 }
 
 features.add(
   import.meta.url,
   init,
-  'Hides percent value in PROD orders list',
+  'PROD: Hides percent value in the order list.',
 );
