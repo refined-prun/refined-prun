a4cea70e94a71fa35438e804683daf8907487e64
CoroNaut <JavaSane2121@yahoo.com>
2025-02-26 11:03:55 -0500

    make the element storeView_container wrap elements instead of having the text wrap

--- src/features/advanced/inv-compress-inventory-info.ts
+++ src/features/advanced/inv-compress-inventory-info.ts
@@ -4,8 +4,6 @@ import classes from './inv-compress-inventory-info.module.css';
 function init() {
   applyScopedClassCssRule('INV', C.StoreView.column, classes.storeViewColumn);
   applyScopedClassCssRule('INV', C.StoreView.container, classes.storeViewContainer);
-  applyScopedClassCssRule('INV', C.StoreView.capacity, classes.storeViewCapacity);
-  applyScopedClassCssRule('INV', C.ProgressBar.container, classes.progressBarContainer);
 }
 
 features.add(import.meta.url, init, 'INV: Compresses specific inventory info into a row.');
