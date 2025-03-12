12463ef6ccdb245f8b2c6ca6b89436bce046cd8b
CoroNaut <JavaSane2121@yahoo.com>
2025-03-11 20:36:50 -0400

    production line is much more reactive an adding new buildings are handled

--- src/features/advanced/prod-collapse-orders/prod-collapse-orders.ts
+++ src/features/advanced/prod-collapse-orders/prod-collapse-orders.ts
@@ -3,6 +3,7 @@ import { sitesStore } from '@src/infrastructure/prun-api/data/sites';
 import { getPrunId } from '@src/infrastructure/prun-ui/attributes';
 import { applyCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
 import { computedTileState } from '@src/store/user-data-tiles';
+import { keepLast } from '@src/utils/keep-last';
 import HiddenIndicator from './HiddenIndicator.vue';
 import HideOrders from './HideOrders.vue';
 import classes from './prod-collapse-orders.module.css';
@@ -34,20 +35,26 @@ async function onTileReady(tile: PrunTile) {
     if (!production) return;
     let columnIndex = 0;
     subscribe($$(grid, C.SiteProductionLines.column), column => {
-      const orders = Array.from(column.children) as HTMLElement[];
+      const orders = computed(() => _$$(column, C.OrderSlot.container));
+      const diviver = _$(column, C.SiteProductionLines.slotDivider)!;
+      if (!diviver) return;
 
-      let line = production.find(line =>
-        line.orders.some(order => order.id === getPrunId(orders[0] as HTMLElement)),
-      )!;
-      if (!line) return;
+      const lineId = production.find(line =>
+        line.orders.some(order => order.id === getPrunId(orders.value[0] as HTMLElement)),
+      )!.id;
+      if (!lineId) return;
+
+      const line = computed(
+        () => productionStore.all.value?.find(prodLine => prodLine.id === lineId)!,
+      );
 
       function setOrdersDisplay(keepCapacity: number, keepSlots: number) {
-        infoProxy.value = [line.type, keepCapacity, keepSlots];
-        orders.forEach((order: HTMLElement, index: number) => {
-          if (index < line.capacity) {
+        infoProxy.value = [line.value.type, keepCapacity, keepSlots];
+        orders.value.forEach((order: HTMLElement, index: number) => {
+          if (index < line.value.capacity) {
             order.style.display = index < keepCapacity ? '' : 'none';
           } else {
-            order.style.display = index <= line.capacity + keepSlots ? '' : 'none';
+            order.style.display = index < line.value.capacity + keepSlots ? '' : 'none';
           }
         });
       }
@@ -55,59 +62,46 @@ async function onTileReady(tile: PrunTile) {
       createFragmentApp(
         HideOrders,
         reactive({
-          headerOrdersInfo: infoProxy.value[line.type],
-          capacity: line.capacity,
-          slots: line.slots,
+          headerOrdersInfo: infoProxy.value[line.value.type],
+          capacity: line.value.capacity,
+          slots: line.value.slots,
           setOrdersDisplay,
           displayAllOrders: () => {
-            orders.forEach(order => order.style.removeProperty('display'));
-            infoProxy.value = [line.type, -1, -1];
+            orders.value.forEach(order => order.style.removeProperty('display'));
+            infoProxy.value = [line.value.type, -1, -1];
           },
         }),
       ).appendTo(_$(grid.children[columnIndex++], C.SiteProductionLines.headerActions)!);
 
       const hiddenProd = document.createElement('div');
-      const hiddenQueue = document.createElement('div');
-
-      function setIndicatorPositions() {
-        orders.at(line.capacity)!.before(hiddenProd);
-        column.appendChild(hiddenQueue);
-      }
-
-      setIndicatorPositions();
-
+      diviver.before(hiddenProd);
       createFragmentApp(
         HiddenIndicator,
         reactive({
           amtHidden: computed(() =>
-            infoProxy.value[line.type] ? line.capacity - infoProxy.value[line.type][0] : 0,
+            infoProxy.value[line.value.type]
+              ? line.value.capacity - infoProxy.value[line.value.type][0]
+              : 0,
           ),
         }),
       ).appendTo(hiddenProd);
 
+      const hiddenQueue = document.createElement('div');
+      keepLast(column, () => column, hiddenQueue);
       createFragmentApp(
         HiddenIndicator,
         reactive({
           amtHidden: computed(() =>
-            infoProxy.value[line.type] ? line.slots - infoProxy.value[line.type][1] : 0,
+            infoProxy.value[line.value.type]
+              ? line.value.slots - infoProxy.value[line.value.type][1]
+              : 0,
           ),
         }),
       ).appendTo(hiddenQueue);
 
-      const lineStore = computed(() =>
-        productionStore.all.value?.find(line => line.id === line.id),
-      );
-      watch(lineStore, (data: PrunApi.ProductionLine | undefined) => {
-        if (!data || (data.capacity === line.capacity && data.slots === line.slots)) return;
-        setIndicatorPositions();
-
-        const newOrders = Array.from(column.children) as HTMLElement[];
-        if (newOrders.length === orders.length) return;
-        orders.length = 0;
-        orders.push(...newOrders);
-
-        line = data;
-        setOrdersDisplay(infoProxy.value[line.type][0], infoProxy.value[line.type][1]);
+      watch(line, () => {
+        diviver.before(hiddenProd);
+        setOrdersDisplay(infoProxy.value[line.value.type][0], infoProxy.value[line.value.type][1]);
       });
     });
   });
