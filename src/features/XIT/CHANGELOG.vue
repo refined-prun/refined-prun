92f06f6924b85f0558e2d095b75639ab23b92d5c
CoroNaut <JavaSane2121@yahoo.com>
2025-03-15 10:11:37 -0400

    refactor and trim file

--- src/features/XIT/CHANGELOG.vue
+++ src/features/XIT/CHANGELOG.vue
@@ -27,47 +27,25 @@ function onClick(selection: number) {
   window.open(selections[selection][1]);
 }
 
-const loading = ref(true);
-const error = ref(false);
+const lists = ['New commands', 'Added', 'Changed', 'Fixed', 'Removed'];
 
-interface Version {
-  version: string;
-  details: {
-    NewCommands?: string[];
-    Added?: string[];
-    Changed?: string[];
-    Fixed?: string[];
-    Removed?: string[];
-  };
-}
-
-function setupInfo(text: string): Partial<Version>[] | undefined {
-  const changelogOutput: Partial<Version>[] = [];
+function setupInfo(text: string) {
+  const changelogOutput: object[] = [];
   const patternVersions = /(## [0-9.Unreleased]+(?:\n(?!(## [0-9.]+|\n*$)).*)*)/g;
   const patternSections = /###\s*(.+?)\n\n([\s\S]*?)(?=###|\s*$)/gs;
   const changelogVersions = [...text.matchAll(patternVersions)].map(match => match[0]);
   for (const section of changelogVersions) {
-    const version: Partial<Version> = {
+    const version = {
       version: section.substring(0, section.indexOf('\n')).replace('## ', ''),
       details: {},
     };
     const changelogSections = [...section.matchAll(patternSections)].map(match => match[0]);
     for (const detail of changelogSections) {
       const info = detail.split('\n').slice(2);
-      if (detail.startsWith('### New commands')) {
-        version.details!.NewCommands = info;
-      }
-      if (detail.startsWith('### Added')) {
-        version.details!.Added = info;
-      }
-      if (detail.startsWith('### Changed')) {
-        version.details!.Changed = info;
-      }
-      if (detail.startsWith('### Fixed')) {
-        version.details!.Fixed = info;
-      }
-      if (detail.startsWith('### Removed')) {
-        version.details!.Removed = info;
+      for (const item of lists) {
+        if (detail.startsWith(`### ${item}`)) {
+          version['details'][item] = info;
+        }
       }
     }
     changelogOutput.push(version);
@@ -76,16 +54,11 @@ function setupInfo(text: string): Partial<Version>[] | undefined {
 }
 
 const patternLink = /`[A-Z ]*`/g;
-interface logType {
-  type: string;
-  text: string;
-}
 
 function processedLog(text: string) {
-  const items: logType[] = [];
+  const items: object[] = [];
   const parts = text.split(patternLink);
   const links = [...text.matchAll(patternLink)].map(match => match[0]);
-
   for (let index = 0; index < parts.length; index++) {
     items.push({
       type: 'span',
@@ -101,19 +74,17 @@ function processedLog(text: string) {
   return items;
 }
 
-const changelogURL =
-  'https://raw.githubusercontent.com/refined-prun/refined-prun/main/CHANGELOG.md';
-
-const changelog = ref<Partial<Version>[]>();
+const changelog = ref<object[]>();
+const loading = ref(true);
 
 async function fetchData() {
-  const response = await fetch(changelogURL);
-
+  const response = await fetch(
+    'https://raw.githubusercontent.com/refined-prun/refined-prun/main/CHANGELOG.md',
+  );
   if (response.status === 200) {
     const text = await response.text();
     changelog.value = setupInfo(text);
   } else {
-    error.value = true;
     console.error('Failed to fetch changelog from refined-prun');
     console.error(`${response.status}`);
   }
@@ -121,18 +92,16 @@ async function fetchData() {
 }
 
 fetchData();
-const TESTVERSION = '25.2.27';
 </script>
 
 <template>
   <LoadingSpinner v-if="loading" :class="$style.loading" />
-  <div v-else-if="!loading && error">Error fetching changelog from refined-prun</div>
-  <div v-else>
+  <div v-else-if="!loading && changelog">
     <table>
       <tr>
         <td colspan="2" :class="$style.header">
           <div>Thanks for using Refined PrUn version: {{ config.version }}</div>
-          <div v-if="changelog![1].version! !== config.version" :class="$style.notCurrentVersion"
+          <div v-if="changelog[1]['version'] !== config.version" :class="$style.notCurrentVersion"
             >You currently don't have the latest version, you may need to update manually.</div
           >
           <div>
@@ -168,27 +137,27 @@ const TESTVERSION = '25.2.27';
           <td
             :key="indexVersion"
             colspan="2"
-            :class="version.version === TESTVERSION ? $style.currentVersion : ''">
+            :class="version['version'] === config.version ? $style.currentVersion : ''">
             <SectionHeader style="display: flex">
-              {{ version.version }}
+              {{ version['version'] }}
             </SectionHeader>
           </td>
         </tr>
-        <tr v-for="(detail, indexDetail) in Object.keys(version.details!)" :key="indexDetail">
+        <tr v-for="(detail, indexDetail) in Object.keys(version['details'])" :key="indexDetail">
           <td
-            :key="version.version"
-            :class="version.version === TESTVERSION ? $style.currentVersion : ''">
+            :key="version['version']"
+            :class="version['version'] === config.version ? $style.currentVersion : ''">
             {{ detail }}
           </td>
-          <td :class="version.version === TESTVERSION ? $style.currentVersion : ''">
-            <template v-for="(log, indexLog) in version.details![detail]" :key="indexLog">
+          <td :class="version['version'] === config.version ? $style.currentVersion : ''">
+            <template v-for="(log, indexLog) in version['details'][detail]" :key="indexLog">
               <div>
                 <template v-for="(item, indexItem) in processedLog(log)">
-                  <template v-if="item.type === 'span'">
-                    <span :key="indexItem"> {{ item.text }}</span>
+                  <template v-if="item['type'] === 'span'">
+                    <span :key="indexItem"> {{ item['text'] }}</span>
                   </template>
-                  <template v-if="item.type === 'PrunLink'">
-                    <PrunLink :key="indexItem" :class="$style.prunLink" :command="item.text" />
+                  <template v-if="item['type'] === 'PrunLink'">
+                    <PrunLink :key="indexItem" :class="$style.prunLink" :command="item['text']" />
                   </template>
                 </template>
               </div>
@@ -198,6 +167,7 @@ const TESTVERSION = '25.2.27';
       </template>
     </table>
   </div>
+  <div v-else-if="!loading">Error fetching changelog from refined-prun</div>
 </template>
 
 <style module>
