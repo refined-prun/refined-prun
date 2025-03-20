ffc7a1436113d3adc441bb64b82a0dc077dc8815
CoroNaut <JavaSane2121@yahoo.com>
2025-03-20 03:49:27 -0400

    make nots-change-format with a dependency on nots-notification-type-label

---
+++ src/features/advanced/nots-change-format.ts
@@ -0,0 +1,24 @@
+import { applyScopedCssRule } from '@src/infrastructure/prun-ui/refined-prun-css';
+import classes from './nots-change-format.module.css';
+import { userData } from '@src/store/user-data';
+
+function init() {
+  if (userData.settings.disabled.find(feature => feature === 'nots-notification-type-label')) {
+    userData.settings.disabled.push('nots-change-format');
+    return;
+  }
+
+  applyScopedCssRule('NOTS', `.${C.AlertListItem.content}`, classes.alertListItemContent);
+  applyScopedCssRule(
+    'NOTS',
+    `.${C.AlertListItem.content} > span:first-of-type`,
+    classes.alertListItemInnerContent,
+  );
+  applyScopedCssRule('NOTS', `.${C.AlertListItem.time}`, classes.alertListItemTime);
+}
+
+features.add(
+  import.meta.url,
+  init,
+  'NOTS: Allow notification contents to take up the full width of the buffer.',
+);
