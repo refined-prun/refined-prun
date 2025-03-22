2bfa73935cd6f7c21e7577d26d2a16589d306e1d
CoroNaut <JavaSane2121@yahoo.com>
2025-03-21 21:11:20 -0400

    different hide and shown buttons

--- src/features/basic/header-hide-controls-button/tile-state.ts
+++
@@ -1,9 +0,0 @@
-import { getTileState as getBaseTileState } from '@src/store/user-data-tiles';
-
-export interface TileState extends UserData.TileState {
-  minimizeContextControls: boolean;
-}
-
-export function getTileState(tile: PrunTile) {
-  return computed(() => getBaseTileState(tile) as TileState);
-}
