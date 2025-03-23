d174e453cdea4b6dd63a9cdc78af8764cd8cbaee
Andrei Andreev <razenpok@outlook.com>
2025-03-23 19:37:01 +0100

    Fix tile.anchor usage

--- src/features/advanced/inv-compress-inventory-info.ts
+++ src/features/advanced/inv-compress-inventory-info.ts
@@ -2,16 +2,17 @@ import { applyScopedClassCssRule } from '@src/infrastructure/prun-ui/refined-pru
 import classes from './inv-compress-inventory-info.module.css';
 
 async function onTileReady(tile: PrunTile) {
-  const column = await $(tile.anchor, C.StoreView.column);
-  const capacities = _$$(column, C.StoreView.capacity);
-  if (capacities.length < 3) {
-    return;
-  }
-  const container = document.createElement('div');
-  container.classList.add(classes.capacityContainer);
-  container.appendChild(capacities[1]);
-  container.appendChild(capacities[2]);
-  capacities[0].after(container);
+  subscribe($$(tile.anchor, C.StoreView.column), column => {
+    const capacities = _$$(column, C.StoreView.capacity);
+    if (capacities.length < 3) {
+      return;
+    }
+    const container = document.createElement('div');
+    container.classList.add(classes.capacityContainer);
+    container.appendChild(capacities[1]);
+    container.appendChild(capacities[2]);
+    capacities[0].after(container);
+  });
 }
 
 function init() {
