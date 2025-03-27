8e59e7cb9cc7208e4465f47b40519636c50dda8c
Andrei Andreev <razenpok@outlook.com>
2025-03-27 12:25:19 +0100

    Simplify and clarify onTileReady

--- src/features/basic/prodco-order-eta.ts
+++ src/features/basic/prodco-order-eta.ts
@@ -1,50 +1,49 @@
 import { productionStore } from '@src/infrastructure/prun-api/data/production';
 import { formatEta } from '@src/utils/format';
-import { refAttributeValue, refTextContent } from '@src/utils/reactive-dom';
+import { refAttributeValue } from '@src/utils/reactive-dom';
 import { createReactiveSpan } from '@src/utils/reactive-element';
+import { observeChildListChanged } from '@src/utils/mutation-observer';
 
 function onTileReady(tile: PrunTile) {
+  const line = computed(() => productionStore.getById(tile.parameter)!);
   subscribe($$(tile.anchor, C.ProductionLine.form), form => {
-    const productionTemplateField = form.children[5];
-    const durationField = form.children[8];
-
-    const dropDownBoxItem = refTextContent(_$(productionTemplateField, C.DropDownBox.currentItem)!);
-    const rcSlider = refAttributeValue(_$(form, 'rc-slider-handle')!, 'aria-valuenow');
-    const line = computed(() => productionStore.getById(tile.parameter)!);
-    let template: PrunApi.ProductionTemplate;
-
-    function getCompletion(needTemplate: boolean = true) {
-      if (needTemplate || !template) {
-        template = getTemplateFromForm(line.value.productionTemplates, form)!;
+    const template = ref<PrunApi.ProductionTemplate>();
+    const templateField = form.children[5];
+    const dropDownItem = _$(templateField, C.DropDownBox.currentItem)!;
+    observeChildListChanged(dropDownItem, () => {
+      // The ProductionLine.template element is re-created each time
+      // a new template in the drop-down box is selected.
+      const templateElement = _$(dropDownItem, C.ProductionLine.template);
+      if (templateElement) {
+        template.value = parseTemplate(line.value, templateElement);
       }
-      return ` (${formatEta(Date.now(), calcCompletionDate(line.value, template, Number(rcSlider.value)))})`;
-    }
-
-    const completionText = ref(getCompletion());
-    watch(dropDownBoxItem, () => {
-      completionText.value = getCompletion();
-    });
-    watch(line, () => {
-      completionText.value = getCompletion(false);
-    });
-    watch(rcSlider, () => {
-      completionText.value = getCompletion(false);
     });
 
+    const orderSizeField = form.children[6];
+    const orderSizeSlider = _$(orderSizeField, 'rc-slider-handle')!;
+    const sliderValue = refAttributeValue(orderSizeSlider, 'aria-valuenow');
+    const orderSize = computed(() => Number(sliderValue.value));
+    const completion = computed(() => {
+      if (!template.value) {
+        return undefined;
+      }
+      return ` (${formatEta(Date.now(), calcCompletionDate(line.value, template.value, orderSize.value))})`;
+    });
+
+    const durationField = form.children[8];
     const durationLabel = _$(durationField, C.StaticInput.static)!;
-    durationLabel.append(createReactiveSpan(durationLabel, completionText));
+    durationLabel.append(createReactiveSpan(durationLabel, completion));
   });
 }
 
-function getTemplateFromForm(templates: PrunApi.ProductionTemplate[], form: HTMLElement) {
-  const template = _$(form, C.ProductionLine.template);
+function parseTemplate(line: PrunApi.ProductionLine, templateElement: HTMLElement) {
   const inputs: [string, number][] = [];
   const outputs: [string, number][] = [];
   // The structure of the template element looks like this:
   // MaterialIcon[], ⇨, MaterialIcon[], duration
   // First, we grab all inputs, and then on ⇨ flip to grabbing outputs.
   let input = true;
-  for (const materials of Array.from(template!.children)) {
+  for (const materials of Array.from(templateElement.children)) {
     if (!materials.classList.contains(C.MaterialIcon.container)) {
       input = false;
       continue;
@@ -58,7 +57,7 @@ function getTemplateFromForm(templates: PrunApi.ProductionTemplate[], form: HTML
     }
   }
 
-  for (const template of templates) {
+  for (const template of line.productionTemplates) {
     const templateInputs = template.inputFactors;
     const templateOutputs = template.outputFactors;
     if (templateInputs.length !== inputs.length || templateOutputs.length !== outputs.length) {
