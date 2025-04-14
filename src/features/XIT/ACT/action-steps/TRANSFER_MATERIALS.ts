fe80c83995aa06abba6e22b11e8e881497451d33
Andrei Andreev <razenpok@outlook.com>
2025-04-14 11:08:46 +0200

    XIT ACT: Fix MTRA action getting stuck when there is no space in the destination inventory

--- src/features/XIT/ACT/action-steps/TRANSFER_MATERIALS.ts
+++ src/features/XIT/ACT/action-steps/TRANSFER_MATERIALS.ts
@@ -2,6 +2,10 @@ import { act } from '@src/features/XIT/ACT/act-registry';
 import { serializeStorage } from '@src/features/XIT/ACT/actions/mtra/utils';
 import { fixed0 } from '@src/utils/format';
 import { changeInputValue, clickElement, focusElement } from '@src/util';
+import { materialsStore } from '@src/infrastructure/prun-api/data/materials';
+import { isDefined } from 'ts-extras';
+import { watchWhile } from '@src/utils/watch';
+import { storagesStore } from '@src/infrastructure/prun-api/data/storage';
 
 interface Data {
   from: PrunApi.Store;
@@ -18,6 +22,22 @@ export const TRANSFER_MATERIALS = act.addActionStep<Data>({
     `from ${serializeStorage(data.from)} to ${serializeStorage(data.to)}`,
   execute: async ctx => {
     const { data, log, setStatus, requestTile, waitAct, waitActionFeedback, complete, fail } = ctx;
+
+    const material = materialsStore.getByTicker(data.ticker);
+    if (!material) {
+      log.error(`Unknown material ${data.ticker}`);
+      fail();
+      return;
+    }
+    const canFitWeight = material.weight <= data.to.weightCapacity - data.to.weightLoad;
+    const canFitVolume = material.volume <= data.to.volumeCapacity - data.to.volumeLoad;
+    if (!canFitWeight || !canFitVolume) {
+      log.warning(
+        `Not enough space in ${serializeStorage(data.to)} to transfer at least 1 ${data.ticker}`,
+      );
+      complete();
+      return;
+    }
 
     const tile = await requestTile(
       `MTRA from-${data.from.id.substring(0, 8)} to-${data.to.id.substring(0, 8)}`,
@@ -69,14 +89,30 @@ export const TRANSFER_MATERIALS = act.addActionStep<Data>({
         `${fixed0(leftover)} ${ticker} not transferred ` +
           `(${fixed0(maxAmount)} of ${fixed0(amount)} transferred)`,
       );
+      if (maxAmount === 0) {
+        complete();
+        return;
+      }
     }
     changeInputValue(amountInput, Math.min(amount, maxAmount).toString());
 
     const transferButton = await $(tile.anchor, C.Button.btn);
 
     await waitAct();
+    const destinationAmount = computed(() => {
+      const store = storagesStore.getById(data.to.id);
+      return (
+        store?.items
+          .map(x => x.quantity ?? undefined)
+          .filter(isDefined)
+          .find(x => x.material.ticker === ticker)?.amount ?? 0
+      );
+    });
+    const currentAmount = destinationAmount.value;
     await clickElement(transferButton);
     await waitActionFeedback(tile);
+    setStatus('Waiting for storage update...');
+    await watchWhile(() => destinationAmount.value === currentAmount);
 
     complete();
   },
