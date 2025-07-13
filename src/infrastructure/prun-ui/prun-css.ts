974c2f94a6912005e1ff73319ac8debf8c35a0bb
Andrei Andreev <Razenpok@outlook.com>
2025-07-13 11:06:11 +0200

    Fix extension load failure if script src is a relative uri

--- src/infrastructure/prun-ui/prun-css.ts
+++ src/infrastructure/prun-ui/prun-css.ts
@@ -70,8 +70,8 @@ export function loadPrunCss() {
 
 export function getPrunCssStylesheets() {
   // Start searching prun styles from the app script to filter out any other injected styles.
-  const appScript = _$$(document.head, 'script').find(x =>
-    x.src?.includes('prosperousuniverse.com'),
+  const appScript = _$$(document.head, 'script').find(
+    x => !x.src.includes('://') || x.src?.includes('prosperousuniverse.com'),
   );
   const valid: HTMLStyleElement[] = [];
   let nextSibling = appScript?.nextElementSibling;
