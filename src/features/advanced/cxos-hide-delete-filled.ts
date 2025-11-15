a047297c7a3b17c5d4427d7fcdb3d0b4553cd375
Andrei Andreev <razenpok@gmail.com>
2025-11-15 20:36:11 +0100

    Simplify and clarify the selector

--- src/features/advanced/cxos-hide-delete-filled.ts
+++ src/features/advanced/cxos-hide-delete-filled.ts
@@ -1,7 +1,10 @@
 import css from '@src/utils/css-utils.module.css';
 
 function init() {
-  applyCssRule('CXOS', `button + .${C.ActionBar.container} .${C.Button.danger}`, css.hidden);
+  // This selector will only trigger when the filters are hidden, since there will be
+  // no other elements between the "clear material filters" button and
+  // the action bar with the "Delete Filled" button.
+  applyCssRule('CXOS', `.${C.Button.btn} + .${C.ActionBar.container}`, css.hidden);
 }
 
 features.add(
