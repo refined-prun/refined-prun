a28c1c88f6b2ca4a1425b8504d8e6e7c295439dd
raylu <90059+raylu@users.noreply.github.com>
2026-04-08 21:57:17 -0700

    contribution-maxed: Don't apply contribution-maxed to POPI
    Only apply to CoGC. It's unusual to max all the POPI;
    most governors want to only upkeep some materials.

--- src/features/basic/contribution-maxed.ts
+++ src/features/basic/contribution-maxed.ts
@@ -17,11 +17,11 @@ function onTileReady(tile: PrunTile) {
 }
 
 function init() {
-  tiles.observe(['COGCU', 'POPID'], onTileReady);
+  tiles.observe(['COGCU'], onTileReady);
 }
 
 features.add(
   import.meta.url,
   init,
-  'Automatically maxes the contribution sliders in CoGC and population upkeep tiles.',
+  'Automatically maxes the contribution sliders in CoGC tiles.',
 );
