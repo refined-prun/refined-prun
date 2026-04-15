a3f4988072aa90e13dea0f622d35894818b00fb2
Chris Woerz <chriswoerz@gmail.com>
2026-04-15 11:20:54 -0600

    Add STO BaseDetail composing StorageTable and VisitationTable

---
+++ src/features/XIT/STO/BaseDetail.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { BaseStorageAnalysis } from '@src/core/storage-analysis';
+import StorageTable from '@src/features/XIT/STO/StorageTable.vue';
+import VisitationTable from '@src/features/XIT/STO/VisitationTable.vue';
+
+defineProps<{ analysis: BaseStorageAnalysis }>();
+</script>
+
+<template>
+  <div :class="$style.detail">
+    <StorageTable :analysis="analysis" />
+    <VisitationTable :analysis="analysis" />
+  </div>
+</template>
+
+<style module>
+.detail {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+  padding: 0.5rem;
+}
+</style>
