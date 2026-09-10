912d2987e3af504a6a3d07c9199a74e5a5dc8169
Andrei Andreev <razenpok@gmail.com>
2026-09-10 23:08:07 +0200

    Exclude tests from glob imports
    Co-authored-by: agm-114 <46271745+agm-114@users.noreply.github.com>

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,5 @@
-import.meta.glob(['./basic/**/*.{ts,tsx}'], { eager: true });
-import.meta.glob(['./advanced/**/*.{ts,tsx}'], { eager: true });
-import.meta.glob(['./XIT/**/*.{ts,tsx}'], { eager: true });
+import.meta.glob(['./basic/**/*.{ts,tsx}', '!./basic/**/*.{test,spec}.{ts,tsx}'], { eager: true });
+import.meta.glob(['./advanced/**/*.{ts,tsx}', '!./advanced/**/*.{test,spec}.{ts,tsx}'], {
+  eager: true,
+});
+import.meta.glob(['./XIT/**/*.{ts,tsx}', '!./XIT/**/*.{test,spec}.{ts,tsx}'], { eager: true });
