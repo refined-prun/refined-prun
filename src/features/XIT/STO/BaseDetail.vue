832613db445fa181f6105d2295d14a9957f7810a
Andrei Andreev <razenpok@gmail.com>
2026-09-10 11:39:28 +0200

    Increase the min size of the grid cell so the visitation frequency table doesn't overflow

--- src/features/XIT/STO/BaseDetail.vue
+++ src/features/XIT/STO/BaseDetail.vue
@@ -256,7 +256,7 @@ const overflowAmount = computed(() => {
 <style module>
 .detail {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
   gap: 0.75rem;
   padding: 0.75rem;
 }
