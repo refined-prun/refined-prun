fb646e6a8d4f72db683635a661636228bcc2b2b7
Andrei Andreev <razenpok@gmail.com>
2026-09-10 16:03:36 +0200

    Rename frame => tileFrame in action-feedback

--- src/infrastructure/prun-ui/utils/action-feedback.ts
+++ src/infrastructure/prun-ui/utils/action-feedback.ts
@@ -1,7 +1,7 @@
 import { clickElement } from '@src/util';
 
-export async function waitActionFeedback(frame: Element) {
-  const overlay = await $(frame, C.ActionFeedback.overlay);
+export async function waitActionFeedback(tileFrame: Element) {
+  const overlay = await $(tileFrame, C.ActionFeedback.overlay);
   if (overlay.classList.contains(C.ActionConfirmationOverlay.container)) {
     const confirm = _$$(overlay, C.Button.btn)[1];
     if (confirm === undefined) {
@@ -10,8 +10,8 @@ export async function waitActionFeedback(frame: Element) {
     await clickElement(confirm);
   }
   const outcome = await Promise.race([
-    $(frame, C.ActionFeedback.error),
-    $(frame, C.ActionFeedback.success),
+    $(tileFrame, C.ActionFeedback.error),
+    $(tileFrame, C.ActionFeedback.success),
   ]);
   if (outcome.classList.contains(C.ActionFeedback.success)) {
     await clickElement(outcome);
